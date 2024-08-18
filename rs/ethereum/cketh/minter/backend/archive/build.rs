21657aac591e98b79a5e13e3895be5ff0215204c
b3hr4d <behradmusiclove@yahoo.com>
2024-08-18 22:10:44 +0300

    refactor: Update CkTokenDeposit.sol to handle deposit of ETH, ERC20 tokens, and NFTs

--- rs/ethereum/cketh/minter/backend/archive/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let did_path = std::path::PathBuf::from("archive.did")
-        .canonicalize()
-        .unwrap();
-
-    println!("cargo:rustc-env=ARCHIVE_DID_PATH={}", did_path.display());
-}
